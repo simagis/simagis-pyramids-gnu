4851fcdb6ad033881a3b2730bb60effab21d6130
Daniel Alievsky <daniel.alievsky@gmail.com>
2014-11-25 16:04:58 +0200

    Loci: automatic installation; pom version changed to snapshot

---
+++ LiveInstaller/install-pp-gnu.cmd
@@ -0,0 +1,25 @@
+@echo off
+set jre64=%ProgramFiles%\Java\jre7
+set jre86=%ProgramFiles(x86)%\Java\jre7
+set liveJarFolder=\pathogensRoot\.imageIO\LivePyramid
+if not exist %liveJarFolder%\PlanePyramid.jar (
+    echo Standard Live module \pathogensRoot\.imageIO\ is not installed properly
+    goto end
+)
+echo Installing additional Simagis Live modules
+echo Already installed Simagis Live libraries %liveJarFolder%\*.jar will be used
+FOR /D /r %%G in (".*") DO (
+    echo %%~nxG
+    if exist \pathogensRoot\%%~nxG\jre (
+        rmdir \pathogensRoot\%%~nxG\jre
+    )
+    xcopy /I/E/Y %%~nxG \pathogensRoot\%%~nxG >nul
+    xcopy /I/E/Y %liveJarFolder% \pathogensRoot\%%~nxG\LivePyramid >nul
+    IF EXIST %%~nxG\x86\con (
+        mklink /J \pathogensRoot\%%~nxG\jre "%jre86%"
+    ) ELSE (
+        mklink /J \pathogensRoot\%%~nxG\jre "%jre64%"
+    )
+)
+echo:
+:end
