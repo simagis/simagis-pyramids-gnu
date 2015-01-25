68f06e61e05f32f1f7c021a0a55743cc59074a98
Daniel Alievsky <daniel.alievsky@gmail.com>
2015-01-25 18:15:51 +0200

    Djatoka: preparing to automatic adding DLL

--- PlanePyramidSourceDjatoka/install_additions.cmd
+++ PlanePyramidSourceDjatoka/install_additions.cmd
@@ -2,6 +2,6 @@
 set java="%ProgramFiles(x86)%\Java\jre7\bin\java.exe"
 set class=com.simagis.executable.installer.ZipExtractor
 set jar=djatoka\adore-djatoka-full-archive-1.1.jar
-%java% -cp LiveAPI/* %class% %jar% adore-djatoka-1.1/bin/Win32/kdu_a60R.dll bin/Windows/kdu_a60R.dll
-%java% -cp LiveAPI/* %class% %jar% adore-djatoka-1.1/bin/Win32/kdu_jni.dll bin/Windows/kdu_jni.dll
-%java% -cp LiveAPI/* %class% %jar% adore-djatoka-1.1/bin/Win32/kdu_v60R.dll bin/Windows/kdu_v60R.dll
+%java% -cp LiveAPI/* %class% %jar% adore-djatoka-1.1/bin/Win32/kdu_a60R.dll bin/Win32/kdu_a60R.dll
+%java% -cp LiveAPI/* %class% %jar% adore-djatoka-1.1/bin/Win32/kdu_jni.dll bin/Win32/kdu_jni.dll
+%java% -cp LiveAPI/* %class% %jar% adore-djatoka-1.1/bin/Win32/kdu_v60R.dll bin/Win32/kdu_v60R.dll
