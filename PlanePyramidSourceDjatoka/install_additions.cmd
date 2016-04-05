0b35022dcab24f8f3bd6c7f5a3e20b761445dfaf
Daniel-Alievsky <daniel.alievsky@gmail.com>
2016-04-05 18:09:04 +0300

    Switching to Java 8 and SDK 1.2.13

--- PlanePyramidSourceDjatoka/install_additions.cmd
+++ PlanePyramidSourceDjatoka/install_additions.cmd
@@ -1,5 +1,5 @@
 @echo off
-set cp=LiveAPI/simagis-plane-pyramid-sdk-1.2.10.jar
+set cp=LiveAPI/simagis-plane-pyramid-sdk-1.2.13.jar
 set class=net.algart.simagis.executable.installer.ZipExtractor
 set jar=djatoka\adore-djatoka-full-archive-1.1.jar
 %java% -cp %cp% %class% %jar% adore-djatoka-1.1/bin/Win32/kdu_a60R.dll kdu_a60R.dll
