db60cc3395b1ddc5bbed07afe2c34898d33869f8
Daniel-Alievsky <daniel.alievsky@gmail.com>
2016-07-05 19:10:24 +0300

    OpenSlide: preparing to add support 0.12.1 (3.4.1)

--- PlanePyramidSourceDjatoka/install_additions.cmd
+++ PlanePyramidSourceDjatoka/install_additions.cmd
@@ -1,5 +1,5 @@
 @echo off
-set cp=simagis-plane-pyramid-executable-1.2.13.jar
+set cp=simagis-plane-pyramid-executable-1.2.15.jar
 set class=net.algart.simagis.executable.installer.ZipExtractor
 set jar=djatoka\adore-djatoka-full-archive-1.1.jar
 %java% -cp %cp% %class% %jar% adore-djatoka-1.1/bin/Win32/kdu_a60R.dll kdu_a60R.dll
